d348d512e2d9fc06bf8e54e4b431ce7db115d89e
henry-bao <henry@bao.nyc>
2023-09-25 22:52:52 +0000

    remove extra comment from commit script

--- commit_docker_compose.sh
+++ commit_docker_compose.sh
@@ -12,5 +12,3 @@ find . -type f -name 'docker-compose.yml' -exec sh -c '
 # Commit the changes
 git commit -m "Add docker-compose files and their containing folders"
 
-# Now, you can push to your remote repository, for example
-# git push origin main
